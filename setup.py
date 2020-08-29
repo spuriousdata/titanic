391a74d19b2096e4e707d74c9fac065d15a4b955
Mike O'Malley <spuriousdata@gmail.com>
2020-08-28 22:36:55 -0400

    add dependency on tqdm

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name="titanic",
-    version='0.0.1',
+    version='0.0.2',
     author="Mike O'Malley",
     author_email="spuriousdata@gmail.com",
     license="MIT",
     packages=['titanic'],
-    install_requires=['PyYAML', 'boto3'],
+    install_requires=['PyYAML', 'boto3', 'tqdm'],
     entry_points={
         'console_scripts': ['titanic=titanic.__main__:main'],
     },
